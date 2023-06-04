d90dda3322f2933d466840a967090f10a565d4c3
Joshua Haddock <joshhaddock04162002@gmail.com>
2023-06-04 12:20:39 +0100

    Finished

--- script.js
+++ script.js
@@ -3,18 +3,54 @@
 //         button.Audio.play(boom.wav)
 //  })
 
-document.getElementById('btn').onclick = function(){
-    //Getting button and listning for click
-    document.getElementById('audio').play();
-    //Utilizing the HTML Audio/Video DOM Reference to play audio/video
-    }
-    
-    //to play on the key A do this(using Jquery):
+let boom = new Audio('/audio/boom.wav')
+let clap = new Audio('/audio/clap.wav')
+let hihat = new Audio('/audio/hihat.wav')
+let kick = new Audio('/audio/kick.wav')
+let openhat = new Audio('/audio/openhat.wav')
+let ride = new Audio('/audio/ride.wav')
+let snare = new Audio('/audio/snare.wav')
+let tink = new Audio('/audio/tink.wav')
+let tom = new Audio('/audio/tom.wav')
+
+
+document.getElementById('boomButton').addEventListener("click", function(){
+    boom.play()
+})
+document.getElementById('clapButton').addEventListener("click", function(){
+    clap.play()
+})
+document.getElementById('hihatButton').addEventListener("click", function(){
+    hihat.play()
+})
+document.getElementById('kickButton').addEventListener("click", function(){
+    kick.play()
+})
+document.getElementById('openhatButton').addEventListener("click", function(){
+    openhat.play()
+})
+document.getElementById('rideButton').addEventListener("click", function(){
+    ride.play()
+})
+document.getElementById('snareButton').addEventListener("click", function(){
+    snare.play()
+})
+document.getElementById('tinkButton').addEventListener("click", function(){
+    tink.play()
+})
+document.getElementById('tomButton').addEventListener("click", function(){
+    tom.play()
+})
+
     document.addEventListener('keydown', function(e) {
+        console.log(e)
       if(e.keyCode == 65){
-      document.getElementById('audio').play();
+      boom.play();
+      }else if (e.keyCode == 83){
+    snare.play();
       }
-    });
+    }
+    );
 
 const insert = document.getElementById('insert');
 const RenderCharacters  = keyData => {
